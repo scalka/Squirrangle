994a50f034a39098c46165ebd8dd41f92f3645a6
scalka <calka.sylwia@gmail.com>
2017-01-26 10:06:19 +0000

    almost done

--- www/js/game.js
+++ www/js/game.js
@@ -2,14 +2,13 @@ var game; // contains game
 var points = 0; // score
 var text, textGameOver; // score text on top of page
 var map; // contains map
-var cursors; 
+var cursors; //keyboard input
 var nutsGroup, trapGroup; // objects layers
 var blockedLayer, blockedlayer, backgroundlayer, backgroundLayer; // layers
 var squirrel, stand, walk, jump, die; // squirrel and movement sprites
 var candy, bin, nut, nuts, pond, mower; // objects
-var hold, up, down, left, right, direction; // flags for movement.
-var pointSound;
-
+var hold, up, down, left, right, direction; // flags for movement
+var pointSound; // sound
 
 window.onload = function () {
     console.log("==onload event");
@@ -21,7 +20,7 @@ window.onload = function () {
     game.state.add("TitleScreen", titleScreen);
     game.state.add("PlayGame", playGame);
     game.state.add("GameOverScreen", gameOverScreen);
-    //kickstart the game with Boot state
+    //start the game with Boot state
     game.state.start("Boot");
 };
 // the boot state is an object with a prototype method and it accepts the game object as an argument, general settings are defined and the assets of the preloading ex. loading bar, nothing is displayed to the user
@@ -69,7 +68,7 @@ var preload = function(game){};
             game.load.image('nutImage', 'asset/objects/nut.png');
             game.load.image('trapImage', 'asset/objects/trap.png');
             //audio
-            game.load.audio('pointAudio', 'asset/audio/sound.mp3', 'asset/audio/sound.mp3');
+            game.load.audio('pointAudio', 'asset/audio/sound.ogg', 'asset/audio/sound.ogg');
 
         },
         create: function(){
@@ -115,9 +114,8 @@ var playGame = function(game){};
             console.log("==playGame. create method");    
             //Start the Arcade Physics systems
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
-            
+            //setting the edges of the world
             this.game.world.setBounds(0, 0, this.game.width, this.game.height);
-            
             // keyboard input
             cursors = game.input.keyboard.createCursorKeys();
             // background - tilemap
@@ -125,42 +123,43 @@ var playGame = function(game){};
             //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
             this.map.addTilesetImage('tiles_spritesheet', 'tiles');
             this.map.addTilesetImage('objects', 'objectsTiles');
-           
             //create layers
             this.backgroundlayer = this.map.createLayer('backgroundLayer');
             this.blockedlayer = this.map.createLayer('blockedLayer');
-            
             //resizes the game world to match the layer dimensions
             this.backgroundlayer.resizeWorld();
             this.backgroundlayer.warp = false;
             //create nuts objects
             this.createNuts();
+            //traps objects
             this.createTraps();
-            
+            //create squirrel
             this.squirrel = game.add.sprite(0, 180, 'squirrangle', 'stand/stand1');
-            
+            //enable physics on squirrel
             this.game.physics.arcade.enable(this.squirrel);
             // world bounds collision
             this.squirrel.body.collideWorldBounds = true;
-            
+            //on click/tap listenSwipe is listenining for swipes
             this.game.input.onDown.add(this.listenSwipe, this);
-
             //camera follows squirrel
             this.game.camera.follow(this.squirrel);
-
-            //text containing points
+            //text containing points on top of the screen
             this.text = game.add.text(10 , 10, "Points: " + points, { font: "45px Arial", fill:  "#FF7256", align: "center" } );
             this.text.fixedToCamera = true;
         },
-        // create nuts objects from tiled map
+        //create nuts objects from tiled map
         createNuts: function(){
+            //adding group of objects
             this.nutsGroup = this.game.add.group();
             this.nutsGroup.enableBody = true;
+            //finding in the json file type of points on the nut layer
             var createNuts = this.findObjectsByType('point', this.map, 'nutsL');
+            //for each of the element with type point exachange it with a nut sprite
             createNuts.forEach(function(element){
                 this.createSpriteFromTiledObject(element, this.nutsGroup, 'nutImage');
             }, this);
         },
+        //similar function as createNuts, but with traps
         createTraps: function(){
             this.trapGroup = this.game.add.group();
             this.trapGroup.enableBody = true;
@@ -186,107 +185,111 @@ var playGame = function(game){};
             Object.keys(element.properties).forEach(function (key) {
                 newSprite[key] == element.properties[key];
             });
-        },      
+        },    
+        //animation for jumping squirrel
         jumpSquirrel: function(){
-            //console.log("==jumpSquirrel");
-                this.squirrel.animations.add('jump', Phaser.Animation.generateFrameNames('jump/jump', 1,3), 10, false);
-                this.squirrel.animations.play('jump');
-                this.squirrel.animations.currentAnim.onComplete.add(this.walkSquirrel, this);
-                this.game.physics.arcade.enable(this.squirrel);             
+            //adding animation with frames based on the json file
+            this.squirrel.animations.add('jump', Phaser.Animation.generateFrameNames('jump/jump', 1,3), 20, false);
+            this.squirrel.animations.play('jump');
+            //on animation complete add walkSwuirrel function
+            this.squirrel.animations.currentAnim.onComplete.add(this.walkSquirrel, this);            
         },
+        //walking squirrel animation
         walkSquirrel: function(){
-            //console.log("==walkSquirrel");
             this.squirrel.canJump = false;
             this.squirrel.canWalk = true;
             if(this.squirrel.canWalk){
                 this.squirrel.animations.add('walk', Phaser.Animation.generateFrameNames('walk/walk', 1,3), 2, true);
                 this.squirrel.animations.play('walk');
-                this.game.physics.arcade.enable(this.squirrel);
             }
         },
+        //function when the squirrel collided with trap
         die: function(squirrel, trap){
-            console.log("trap");
-            
             this.squirrel.body.bounce.y = 0.2;
             this.squirrel.body.gravity.y = 2000;
             this.squirrel.body.gravity.x = 20;
             this.squirrel.body.velocity.x = 100;
-            game.time.events.add(Phaser.Timer.SECOND*2, function() {
+            game.time.events.add(Phaser.Timer.SECOND, function() {
                 game.state.start("GameOverScreen");
             });
         }, 
-        
+        //update function
         update: function(){
-            //console.log("===update function")
+            //listening for swipes
            this.listenSwipe(function(direction) {
-                console.log("outside of if listen swipe: " + direction);
                 return direction;
-            });      
+            });
+            //setting collisions between swuirrel and nuts and traps
             this.game.physics.arcade.overlap(this.squirrel, this.nutsGroup, this.collision, null, this);
             this.game.physics.arcade.overlap(this.squirrel, this.trapGroup, this.die, null, this);
-           
+            //updating points text
             this.text.setText("Points: " + points);
-
+            //listening for tap/click event
             if (this.game.input.activePointer.isDown){
                 this.game.input.onDown.add(this.jumpSquirrel, this);
-                this.squirrel.x +=2;
+                //increase velocity on tap
+                this.squirrel.body.velocity.x +=2;
             }
-            
+            //on click up listening for swipe
             this.game.input.onUp.add(this.listenSwipe, this);
         },
-        
+        //functions which is detecting swipes
         listenSwipe: function(callback) {
             var eventDuration;
             var startPoint = {};
-            var endPoint = {};    
+            var endPoint = {};  
+            //min duration and distance of swipe
             var minimum = {
                  duration: 10,
                  distance: 10
             }   
-
+            //catching coordinates on down event
             this.game.input.onDown.add(function(pointer) {
                 startPoint.x = pointer.clientX;     
                 startPoint.y = pointer.clientY; 
             }, this);
-            
+            //catching coordinates on up event
             this.game.input.onUp.add(function(pointer) {     
                 direction = '';     
                 eventDuration = game.input.activePointer.duration;    
-                if (eventDuration > minimum.duration) {
-                        console.log(eventDuration);                    
+                if (eventDuration > minimum.duration) {                  
                     endPoint.x = pointer.clientX;           
-                    endPoint.y = pointer.clientY;           // Check direction   
+                    endPoint.y = pointer.clientY;           
+                    // Check direction   
                     if (endPoint.y - startPoint.y > minimum.distance) { 
                         direction = 'bottom';  
-                        this.squirrel.y += 0.5;     
-                        console.log("swipe bottom")
-                        
+                        //move down
+                        this.squirrel.y += 0.2;
+                        //reset velocity
+                        this.squirrel.body.velocity.x = 0;                    
                     } else if (startPoint.y - endPoint.y > minimum.distance) {           
                         direction = 'top'; 
-                        console.log("top");
-                        this.squirrel.y -= 0.5;   
+                        //reset velocity
+                        this.squirrel.body.velocity.x = 0;
+                        //move up
+                        this.squirrel.y -= 0.2;   
                     }     
                 }
             }, this);
         },  
-
+        //collision function
         collision: function(squirrel, object){
+            //play sound
             this.pointSound = this.game.add.audio('pointAudio');
-            console.log("collision");
-            points += 1;
             this.pointSound.play();
-            
+            //increase points
+            points += 1;
+            //destroy nut
             object.destroy();
-        },
-
-        
+        }        
  }
+//game over state and screen    
 var gameOverScreen = function (game){};
     gameOverScreen.prototype = {
         //Checking Collisions
         create: function(){
             console.log("==gameOverScreen state. Create method");
-            this.text = game.add.text(200 , game.height/2, "Game Over: " + points + " points", { font: "45px Arial", fill:  "#FF7256", align: "center" } );        
+            this.text = game.add.text(100 , game.height/2, "GAME OVER: " + points + " points", { font: "45px Source Sans Pro", fill:  "#fff200", align: "center" } );        
         }
 };
     
